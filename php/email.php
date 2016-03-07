d80d92c3a34ce22e16b99759cc2151b34fb51e21
Taras <rudyktaras@gmail.com>
2016-03-07 10:28:45 +0000

    initial commit

--- php/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $from = "trudyk@trudyk.net23.net";
-    $to = "rudyktaras@gmail.com";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    mail ($to, $name, $email,$message);
-?>
